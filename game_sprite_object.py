43ecb0f43d7f4066f1e0cf009f1f75598c746e12
paalso <40a.pvl@gmail.com>
2021-06-28 12:58:35 +0300

    Refactor game structure

--- game_sprite_object.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from game_object import GameObject
-
-
-class GameSpriteObject(pygame.sprite.Sprite, GameObject):
-
-    def __init__(self, settings, screen):
-        pygame.sprite.Sprite.__init__(self)
-        GameObject.__init__(self, settings, screen)
